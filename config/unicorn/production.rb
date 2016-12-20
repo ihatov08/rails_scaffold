c617537c56dcdea191a6cff0923809d83d15472a
tomoya <ihatov08@gmail.com>
2016-12-21 02:43:51 +0900

    [modify]unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,32 +1,27 @@
-  # set lets
-  $worker  = 2
-  $timeout = 30
-  $app_dir = "/home/tomoya/rails/rails_scaffold" #自分のアプリケーション名
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
-  # set config
-  worker_processes  $worker
-  working_directory $app_dir
-  stderr_path $std_log
-  stdout_path $std_log
-  timeout $timeout
-  listen  $listen
-  pid $pid
+@app_path = '/home/tomoya/rails'
+working_directory @app_path + "/current"
+$app_dir = "/home/tomoya/rails/rails_scaffold" #worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+preload_app true
+timeout 30
+listen "#{@app_path}/current/tmp/sockets/.unicorn.sock", :backlog => 64
+pid "#{@app_path}/current/tmp/pids/unicorn.pid"
+
+stderr_path "#{@app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{@app_path}/shared/log/unicorn.stdout.log"
   # loading booster
-  preload_app true
-  # before starting processes
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
-      begin
-        Process.kill "QUIT", File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+preload_app true
+# before starting processes
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-  # after finishing processes
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+end
+# after finishing processes
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
